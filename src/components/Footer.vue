384c721955cb5c0208f037d9fec2f57d065a6e08
ariellecharlize-cp <ariellecharlize.castillo@coreproc.com>
2026-05-28 12:06:36 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
         <div class="max-w-6xl mx-auto px-6 py-4 flex flex-col items-center justify-center text-center">
         
         <!--icon-->
-        <div class="flex flex-col md:flex-row items-center justify-between gap- text-center md:text-left">
+        <div class="flex flex-col md:flex-row items-center justify-between gap-1 md:gap-50 text-center md:text-left">
             <div class="flex items-center gap-2">
                 <div class="w-8 h-8 bg-emerald rounded-lg flex items-center justify-between p-1.5 text-white font-black text-center">
                     C
@@ -12,13 +12,13 @@
                  <span class="text-xl text-emerald-light font-bold tracking-tight"> CoreCRM</span>
             </div>
 
-            <div class="grid grid-cols-3 gap-4 md:flex-row items-center justify-center gap-10 mt-10 md:mt-0 text-emerald-light text-sm">
+            <div class="grid grid-cols-3 gap-4 md:flex-row items-center justify-center gap-10 mt-10 md:mt-0 text-emerald-pale text-sm font-bold">
                 <p>Privacy</p>
                 <p>Terms</p>
                 <p>Support</p>
             </div>
             
-            <p class="text-emerald-light text-sm mt-4 md:mt-0">
+            <p class="text-emerald-pale text-sm mt-4 md:mt-0">
                 &copy; 2026 CoreCRM. All rights reserved.
             </p>
 
